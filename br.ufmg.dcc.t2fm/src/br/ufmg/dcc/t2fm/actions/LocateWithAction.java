e0f875fb9dd5091d9d79049af944df1a97ebe61a
Alcemir Santos <alcemir.santos@gmail.com>
2012-07-11 17:11:24 -0300

    adicionei menu como no cap5-GAMMA. crei a classe cap6-GAMMA.

---
+++ br.ufmg.dcc.t2fm/src/br/ufmg/dcc/t2fm/actions/LocateWithAction.java
@@ -0,0 +1,53 @@
+/*************************************************************************
+ * Copyright (c) 2012 Federal University of Minas Gerais - UFMG 
+ * All rights avaiable. This program and the accompanying materials
+ * are made avaiable under the terms of the Eclipse Public Lincense v1.0
+ * which accompanies this distribution, and is avaiable at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Alcemir R. Santos - improvements on the ConcernMapper
+ * 			architeture. ConcernMapper is available at
+ * 			http://www.cs.mcgill.ca/~martin/cm/
+ *************************************************************************/
+package br.ufmg.dcc.t2fm.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+/**
+ * @author Alcemir R. Santos
+ *
+ */
+public class LocateWithAction implements IObjectActionDelegate {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	ISelection selection;
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction, org.eclipse.ui.IWorkbenchPart)
+	 */
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
