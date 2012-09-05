c49ac4860742b82ba1acd4b84b286f69a6c8cd2b
Alcemir Santos <alcemir.santos@gmail.com>
2012-09-05 18:43:24 -0300

    adicionei os métodos de impressão do que tem nos projetos como no tutorial do vogella.com

--- br.ufmg.dcc.t2fm/src/br/ufmg/dcc/t2fm/actions/SaveCoverageAsCMAction.java
+++ br.ufmg.dcc.t2fm/src/br/ufmg/dcc/t2fm/actions/SaveCoverageAsCMAction.java
@@ -12,27 +12,41 @@
  *************************************************************************/
 package br.ufmg.dcc.t2fm.actions;
 
-import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.text.Document;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
-import org.jacoco.core.analysis.ICoverageNode;
 
 import br.ufmg.dcc.t2fm.views.MapView;
 
 import com.mountainminds.eclemma.core.CoverageTools;
 import com.mountainminds.eclemma.core.ICoverageSession;
-import com.mountainminds.eclemma.core.ScopeUtils;;
+import com.mountainminds.eclemma.core.ScopeUtils;
 
 /**
  * @author Alcemir R. Santos
- *
+ * 
  */
 public class SaveCoverageAsCMAction extends Action {
 
 	MapView aView;
+
 	/**
 	 * @param mapView
 	 */
@@ -40,32 +54,119 @@ public class SaveCoverageAsCMAction extends Action {
 		this.aView = mapView;
 		setText("Save Coverage into a cm File");
 		setToolTipText("Save Coverage into a cm File");
-		setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJ_FILE));
+		setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJ_FILE));
 	}
 
-	public void run(){
-	
+	public void run() {
+
 		boolean thereIsActiveCoverageSession = false;
-		ICoverageSession activeSession = CoverageTools.getSessionManager().getActiveSession();
-		thereIsActiveCoverageSession =  activeSession == null ? false:true ;
-		
+		ICoverageSession activeSession = CoverageTools.getSessionManager()
+				.getActiveSession();
+		thereIsActiveCoverageSession = activeSession == null ? false : true;
+
+		HashSet<IPackageFragmentRoot> escopo = null;
+
 		// se existe uma sessão ativa
-		if(thereIsActiveCoverageSession ){
-			Set<IPackageFragmentRoot> escopo =  ScopeUtils.filterJREEntries( activeSession.getScope() );
-			//&& e se o elemento está no escopo
-			for(IPackageFragmentRoot pakkage: escopo){
-				CoverageTools.getCoverageInfo(object);
-				ICoverageNode coverage = (ICoverageNode) someJavaElement.getAdapter(ICoverageNode.class);
+		if (thereIsActiveCoverageSession) {
+			try {
+				escopo = (HashSet<IPackageFragmentRoot>) ScopeUtils
+						.filterJREEntries(activeSession.getScope());
+			} catch (JavaModelException e) {
+				e.printStackTrace();
+			}
+
+			// && e se o elemento está no escopo
+			Iterator<IPackageFragmentRoot> iterator = escopo.iterator();
+			while (iterator.hasNext()) {
+				IPackageFragmentRoot fragmentRoot = iterator.next();
+				String s = fragmentRoot.getPath().lastSegment();
+				IJavaModel ijm = fragmentRoot.getJavaModel();
+				IWorkspace workspace = ijm.getWorkspace();
+				IWorkspaceRoot root = workspace.getRoot();
+				// Get all projects in the workspace
+				IProject[] projects = root.getProjects();
+				// Loop over all projects
+				for (IProject project : projects) {
+					try {
+						printProjectInfo(project);
+					} catch (CoreException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println(s);
+
+				// CoverageTools.getCoverageInfo(object);
+				// ICoverageNode coverage = (ICoverageNode)
+				// someJavaElement.getAdapter(ICoverageNode.class);
 				// TODO criar um concernmodel com o que foi coberto;
 			}
-			
-			
-		}
-		
 
-		
+		}
+
 		// TODO escrever no cm file;
 	}
-	
 
+	private void printProjectInfo(IProject project) throws CoreException,
+			JavaModelException {
+		System.out.println("Working in project " + project.getName());
+		// Check if we have a Java project
+		if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+			IJavaProject javaProject = JavaCore.create(project);
+			printPackageInfos(javaProject);
+		}
+	}
+
+	private void printPackageInfos(IJavaProject javaProject)
+			throws JavaModelException {
+		IPackageFragment[] packages = javaProject.getPackageFragments();
+		for (IPackageFragment mypackage : packages) {
+			// Package fragments include all packages in the
+			// classpath
+			// We will only look at the package from the source
+			// folder
+			// K_BINARY would include also included JARS, e.g.
+			// rt.jar
+			if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+				System.out.println("Package " + mypackage.getElementName());
+				printICompilationUnitInfo(mypackage);
+
+			}
+
+		}
+	}
+
+	private void printICompilationUnitInfo(IPackageFragment mypackage)
+			throws JavaModelException {
+		for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+			printCompilationUnitDetails(unit);
+
+		}
+	}
+
+	private void printIMethods(ICompilationUnit unit) throws JavaModelException {
+		IType[] allTypes = unit.getAllTypes();
+		for (IType type : allTypes) {
+			printIMethodDetails(type);
+		}
+	}
+
+	private void printCompilationUnitDetails(ICompilationUnit unit)
+			throws JavaModelException {
+		System.out.println("Source file " + unit.getElementName());
+		Document doc = new Document(unit.getSource());
+		System.out.println("Has number of lines: " + doc.getNumberOfLines());
+		printIMethods(unit);
+	}
+
+	private void printIMethodDetails(IType type) throws JavaModelException {
+		IMethod[] methods = type.getMethods();
+		for (IMethod method : methods) {
+
+			System.out.println("Method name " + method.getElementName());
+			System.out.println("Signature " + method.getSignature());
+			System.out.println("Return Type " + method.getReturnType());
+
+		}
+	}
 }
